4625a6c24563af2046d91f177ed836e04b55ace5
anhDean <tung.dinh90@gmail.com>
2016-03-02 23:46:39 +0100

    Minor Changes in FileHandler Class
    - template member function definitions in header file
    - template function signature corrected -> compile successfull

--- FileHandler.h
+++ FileHandler.h
@@ -30,12 +30,11 @@ private:
 public:
 	// TODO: new constructor for videos -> use cv:: videocapture FileHandler(captureobject, outputdirectory)
 	// TODO: process_video member function -> for processing videos cv::Mat getFrame() -> process -> write
-	template<typename FrameProcessorClass>
+	template<class FrameProcessorClass>
 	bool process_folder(std::string inputFolder, std::string outputFolder, FrameProcessorClass& processor) const;
 	
-	template<typename FrameProcessorClass>
-	bool FileHandler::process_folder(int idx, FrameProcessorClass& processor) const;
-
+	template<class FrameProcessorClass>
+	bool process_folder(int idx, FrameProcessorClass& processor) const;
 
 	FileHandler(const std::string& inpRt, const std::string& outRt, int rt_depth);
 	const int& getFolderCount() const;
@@ -45,5 +44,56 @@ public:
 	bool setOutputSuffix(const std::string& newVal); // must contain ".", e.g. ".jpg"
 	void setInputPrefix(const std::string& newVal);
 	void setOutputPrefix(const std::string& newVal);
+
 };
 
+// template member functions defined in header
+
+template<class FrameProcessorClass>
+bool FileHandler::process_folder(std::string inputFolder, std::string outputFolder, FrameProcessorClass& processor) const
+{
+	// get file name
+	std::string tmp_inputFile, tmp_outputFile;
+	cv::Mat input, result;
+
+	for (fs::directory_iterator it(inputFolder), end; it != end; ++it)
+	{
+
+		if (fs::is_regular_file(it->path()) && (it->path().filename().string().find(m_inputSuffix) != std::string::npos))
+		{
+			tmp_inputFile = it->path().filename().string();
+			tmp_outputFile = getOutputFilename(tmp_inputFile);
+		}
+		input = cv::imread(tmp_inputFile, CV_LOAD_IMAGE_COLOR);
+		result.create(input.size(), CV_8U);
+		processor->process(input, result);
+
+		cv::imwrite(outputFolder + "\\" + tmp_outputFile, result);
+	}
+
+	return true;
+}
+
+template<class FrameProcessorClass>
+bool FileHandler::process_folder(int idx, FrameProcessorClass& processor) const
+{
+	// get file name
+	std::string tmp_inputFile, tmp_outputFile;
+	cv::Mat input, result;
+
+	for (fs::directory_iterator it(m_inputFolders[idx]), end; it != end; ++it)
+	{
+
+		if (fs::is_regular_file(it->path()) && (it->path().filename().string().find(m_inputSuffix) != std::string::npos))
+		{
+			tmp_inputFile = it->path().filename().string();
+			tmp_outputFile = getOutputFilename(tmp_inputFile);
+		}
+		input = cv::imread(tmp_inputFile, CV_LOAD_IMAGE_COLOR);
+		result.create(input.size(), CV_8U);
+		processor->process(input, result);
+
+		cv::imwrite(m_outputFolders[idx] + "\\" + tmp_outputFile, result);
+	}
+	return true;
+}
